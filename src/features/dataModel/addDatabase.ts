6e168e0a884b2b1d313d856aaca746f16c3852ca
Onyx Ganda <onyxganda@gmail.com>
2022-08-22 13:01:39 +0200

    addDatabase action and require methods

---
+++ src/features/dataModel/addDatabase.ts
@@ -0,0 +1,18 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import getDatabaseList from './getDatabaseList';
+import DbManager from '../../utils/dbManager';
+import LocalStorageList from '../../utils/localStorage/localStorage';
+
+const action = createAsyncThunk(
+    'addDatabase',
+    async ( name: string, thunkApi ) => {
+        let db = new DbManager(name);
+        db = await DbManager.build(db);
+        let databaseList = new LocalStorageList('databaseList').addElement(db.getName());
+        debugger;
+        await thunkApi.dispatch(getDatabaseList(true));
+        return name;
+    }
+)
+
+export default action;
