6ee15fa5048baf8863726eb6082db78910e3d900
Onyx Ganda <onyxganda@gmail.com>
2022-09-01 15:29:26 +0200

    EntiŧyAccordion component improvements (removed test data)

--- src/components/commons/EntityAccordion/index.tsx
+++ src/components/commons/EntityAccordion/index.tsx
@@ -2,45 +2,30 @@ import React from 'react';
 
 import { Collapse } from 'antd';
 
+
 require('antd/lib/collapse/style');
 
 interface EntityAccordionProps {
+    list: { name: String; content?: any }[];
+    injectedContent?: JSX.Element;
+    onChange?: (...arg: any) => void;
     extra?: JSX.Element; // will allow buttons at the right of the accordions header
+    getExtra?: ( ...arg: any) => JSX.Element;
 }
 const EntityAccordion = (props: EntityAccordionProps) => {
-    const { extra } = props;
-    const onChange = (key: string | string[], data: { name: string }[]) => {
-        // TODO: load info data on request and dispatch action to add it to corresponding
-        // obj in databaseList?
-        if (key) {
-            for ( const singleKey of [...key] ) {
-                console.log("Got selected element(s):", data[Number(singleKey)]);
-            }
-        }
-    };
-
-    const testData = [
-        { name: 'Title 1' },
-        { name: 'Title 2' },
-        { name: 'Title 3' },
-        { name: 'Title 4' },
-        { name: 'Title 5' },
-    ]
-
-    const text = `
-    A dog is a type of domesticated animal.
-    Known for its loyalty and faithfulness,
-    it can be found as a welcome guest in many households across the world.
-    `;
+    const { extra, list, injectedContent, onChange, getExtra } = props;
 
     return (
         <Collapse accordion={true}
             defaultActiveKey={['1']}
-            onChange={(key: string | string[]) => onChange(key, testData)}>
-            {testData.map((el, index) => {
+            onChange={(key: string | string[]) => onChange && onChange(key)}>
+            {list?.map((el, index) => {
                 return (
-                    <Collapse.Panel header={el.name} key={index} extra={extra}>
-                        <p>{text}</p>
+                    <Collapse.Panel 
+                        header={el.name} key={index}
+                        extra={extra || (getExtra && getExtra(el.name))}
+                    >
+                        <p>{el?.content || injectedContent}</p>
                     </Collapse.Panel>
                 )
             })}
